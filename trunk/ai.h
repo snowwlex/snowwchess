294776eeb54757e55f1cba13234ea35b26bef956
snowwlex <snowwlex@ea7e4570-1e0a-11df-ae22-3371deb0c1a1>
2010-03-14 02:55:44 +0000

    Fixed issue 18.

--- trunk/ai.h
+++ trunk/ai.h
@@ -26,4 +26,29 @@ class AIPlayer : public Player {
 		virtual PlayerCommand makeTurn(Move& move, GameMessage message = NONE);
 };
 
+class AlphaBetaAIPlayer : public Player {
+	private:
+		struct AlphaBetaBorder {
+			bool isLower;
+			bool isUpper;
+			int lower;
+			int upper;
+			AlphaBetaBorder() { isLower=false; isUpper = false; lower=0; upper=0; }
+		};
+		int myColor;
+		int myTurnsCounter;
+		int myDepth;
+		int myCounter;
+		Model *myModel;
+		BoardCLIView *myBoardView;
+		CLIView *myUserView;
+
+		Move search();
+		int searchRecurs(Model m, Move move, int curDepth, int max, AlphaBetaBorder borders);
+		int sef(Model *m) const;
+		bool alphaBetaPruning(int max, AlphaBetaBorder & borders, int score);
+	public:
+		AlphaBetaAIPlayer(int color, Model* m, BoardCLIView *boardView, CLIView * userView, int depth);
+		virtual PlayerCommand makeTurn(Move& move, GameMessage message = NONE);
+};
 #endif /* AI_H_ */
