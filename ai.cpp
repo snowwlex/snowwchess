09aacb99695e9c30ea266b095daa837f6aa1e160
snowwlex <snowwlex@ea7e4570-1e0a-11df-ae22-3371deb0c1a1>
2010-03-20 00:03:02 +0000

    Added null window search to imrpove AB prunings (nega scout algorithm) Fixed issue 21.

--- ai.cpp
+++ ai.cpp
@@ -43,10 +43,10 @@ int AIPlayer::sefMaterial(const Model& model, int player) const {
 }
 
 
-BruteForceAIPlayer::BruteForceAIPlayer(int color, Model* model, BoardCLIView *boardView, CLIView * userView, int depth):
+FullSearchAIPlayer::FullSearchAIPlayer(int color, Model* model, BoardCLIView *boardView, CLIView * userView, int depth):
 		AIPlayer(color,model,boardView,userView,depth) { }
 
-PlayerCommand BruteForceAIPlayer::makeTurn(Move& move, GameMessage message) {
+PlayerCommand FullSearchAIPlayer::makeTurn(Move& move, GameMessage message) {
 
 	PlayerCommand command;
 	int score;
@@ -77,7 +77,7 @@ PlayerCommand BruteForceAIPlayer::makeTurn(Move& move, GameMessage message) {
 }
 
 
-int BruteForceAIPlayer::miniMaxSearch(Move& returnMove, int curDepth, int curPlayer, const Model& model) {
+int FullSearchAIPlayer::miniMaxSearch(Move& returnMove, int curDepth, int curPlayer, const Model& model) {
 	MOVES moves;
 	Move bestMove;
 	MOVES::iterator itMove;
@@ -113,7 +113,7 @@ int BruteForceAIPlayer::miniMaxSearch(Move& returnMove, int curDepth, int curPla
 			if ( (curPlayer == myColor && tmpScore > score) || ( curPlayer != myColor && tmpScore < score) ) {
 				score = tmpScore;
 				returnMove = *itMove;
-			} else if (tmpScore == score && rand()%2 ) {
+			} else if (false && tmpScore == score && rand()%2 ) {
 				returnMove = *itMove;
 			}
 		}
@@ -122,10 +122,10 @@ int BruteForceAIPlayer::miniMaxSearch(Move& returnMove, int curDepth, int curPla
 }
 
 
-AlphaBetaAIPlayer::AlphaBetaAIPlayer(int color, Model* model, BoardCLIView *boardView, CLIView * userView, int depth):
+AlphaBetaSearchAIPlayer::AlphaBetaSearchAIPlayer(int color, Model* model, BoardCLIView *boardView, CLIView * userView, int depth):
 		AIPlayer(color,model,boardView,userView,depth) { }
 
-PlayerCommand AlphaBetaAIPlayer::makeTurn(Move& move, GameMessage message) {
+PlayerCommand AlphaBetaSearchAIPlayer::makeTurn(Move& move, GameMessage message) {
 
 	PlayerCommand command;
 	int score;
@@ -158,7 +158,7 @@ PlayerCommand AlphaBetaAIPlayer::makeTurn(Move& move, GameMessage message) {
 }
 
 
-int AlphaBetaAIPlayer::alphaBetaNegaMaxSearch(Move& returnMove, Border alpha, Border beta, int curPlayer, int curDepth , const Model& model) {
+int AlphaBetaSearchAIPlayer::alphaBetaNegaMaxSearch(Move& returnMove, Border alpha, Border beta, int curPlayer, int curDepth , const Model& model) {
 	MOVES moves;
 	Move bestMove;
 	MOVES::iterator itMove;
@@ -178,20 +178,38 @@ int AlphaBetaAIPlayer::alphaBetaNegaMaxSearch(Move& returnMove, Border alpha, Bo
 
 	//fputs(std::string(3*(myDepth-curDepth),' ').c_str(),statfile); sprintf(buffer,"[%d] BEFORE: %d:%d\n",curDepth, alpha.myIsInfinity==0?alpha.myValue:999, beta.myIsInfinity==0?beta.myValue:999); fputs(buffer,statfile);
 	Border score(0, -INF);
+
 	for (pruning = false, itMove = moves.begin(); !pruning &&  itMove != moves.end(); ++itMove) {
 
 		Model curModel = model;
 		curModel.makeMove(*itMove);
-		tmpScore = -alphaBetaNegaMaxSearch(bestMove, -beta,-alpha,1-curPlayer, curDepth-1,curModel);
-		if (curDepth == myDepth) {
-			sprintf(buffer,"[AB] %c%c-%c%c, eff=%d, type=%d,pl=%d, '%c' SCORE %d]\n",itMove->pos1.myX+'a',myModel->getBoardSizeY() - itMove->pos1.myY + '0',itMove->pos2.myX+'a',myModel->getBoardSizeY() - itMove->pos2.myY + '0',itMove->effect,itMove->type,itMove->player,myModel->getFigureData(itMove->figureId).letter,tmpScore); debugView->render(buffer);
-			//debugView->wait();
+
+
+		if ( alpha.myIsInfinity == 0) {
+			// null window search
+			Border newAlpha = alpha;
+			newAlpha.myValue += 1;
+			tmpScore = -alphaBetaNegaMaxSearch(bestMove, -newAlpha,-alpha,1-curPlayer, curDepth-1,curModel);
+			if (alpha < tmpScore && beta > tmpScore) {
+				tmpScore = -alphaBetaNegaMaxSearch(bestMove, -beta,-alpha,1-curPlayer, curDepth-1,curModel);
+			}
+			if (curDepth == myDepth) {
+				sprintf(buffer,"[++] %c%c-%c%c, eff=%d, type=%d,pl=%d, '%c' SCORE %d]\n",itMove->pos1.myX+'a',myModel->getBoardSizeY() - itMove->pos1.myY + '0',itMove->pos2.myX+'a',myModel->getBoardSizeY() - itMove->pos2.myY + '0',itMove->effect,itMove->type,itMove->player,myModel->getFigureData(itMove->figureId).letter,tmpScore); debugView->render(buffer);
+				//debugView->wait();
+			}
+		} else {
+			tmpScore = -alphaBetaNegaMaxSearch(bestMove, -beta,-alpha,1-curPlayer, curDepth-1,curModel);
+			if (curDepth == myDepth) {
+				sprintf(buffer,"[AB] %c%c-%c%c, eff=%d, type=%d,pl=%d, '%c' SCORE %d]\n",itMove->pos1.myX+'a',myModel->getBoardSizeY() - itMove->pos1.myY + '0',itMove->pos2.myX+'a',myModel->getBoardSizeY() - itMove->pos2.myY + '0',itMove->effect,itMove->type,itMove->player,myModel->getFigureData(itMove->figureId).letter,tmpScore); debugView->render(buffer);
+				//debugView->wait();
+			}
 		}
 
 		//if (result == score && rand()%2) { returnMove = *itMove;}
-		if (score < tmpScore)  { score = tmpScore; returnMove = *itMove; }
+		if (score  < tmpScore)  { score = tmpScore; returnMove = *itMove; }
 		if (alpha  < score)  alpha = score;
-		if (beta   < alpha) pruning = true;
+		if (beta   < alpha ) pruning = true;
+		//if (/*curDepth < myDepth &&*/  beta == alpha) pruning = true;
 
 		//fputs(std::string(3*(myDepth-curDepth),' ').c_str(),statfile); sprintf(buffer,"[%d] AFTER: result=%d, %d:%d\n",curDepth, result.myIsInfinity==0?result.myValue:999,alpha.myIsInfinity==0?alpha.myValue:999, beta.myIsInfinity==0?beta.myValue:999);fputs(buffer,statfile);
 
